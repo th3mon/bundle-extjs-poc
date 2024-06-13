b3981387796e6f0136b07071034649c5d5e98d18
Przemyslaw Szelenberger <przemyslaw.szelenberger@cloudgpw.pl>
2024-06-13 15:39:15 +0200

    Manual dist

--- dist/app-bundle.js
+++ dist/app-bundle.js
@@ -1,7 +1,30 @@
+Ext.define("components.buttons.ShowMessage", {
+  extend: "Ext.Button",
+  alias: "button.ShowMessage",
+  text: "Show Message",
+  handler() {
+    Ext.Msg.alert("Message", "42!");
+  },
+});
+
+Ext.define("components.main-panel.MainPanel", {
+  extend: "Ext.panel.Panel",
+  alias: "widget.MainPanel",
+  title: "Sample App",
+  html: "The sample app",
+  layout: "fit",
+  border: false,
+  items: [Ext.create("components.buttons.ShowMessage")],
+});
+
 const App = () => {
   Ext.create("components.main-panel.MainPanel", {
     renderTo: Ext.getBody(),
   });
 };
 
-App();
+try {
+  App();
+} catch {
+  Ext.onReady(() => App());
+}
