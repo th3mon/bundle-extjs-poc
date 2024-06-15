25b7b5db993d31db0af5af8605852a66b81146ed
Przemyslaw Szelenberger <przemyslaw.szelenberger@cloudgpw.pl>
2024-06-15 03:23:34 +0200

    Bundle stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
-const gulp = require("gulp");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const { src, dest, parallel } = require("gulp");
 
-gulp.task("default", () => {
-  console.log("default task");
+function jsBundle(cb) {
+  const jsFiles = ["src/**/*.js", "!src/boot.js"];
 
-  return true;
-});
+  src(jsFiles).pipe(concat("app-bundle.js")).pipe(uglify()).pipe(dest("dist"));
 
-gulp.task("js", () => {
-  return gulp
-    .src("src/**/*.js")
-    .pipe(concat("app-bundle.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest("dist"));
-});
+  cb();
+}
+
+function copyVendorFiles(cb) {
+  src("vendors/**/*.*").pipe(dest("dist/vendors"));
+
+  cb();
+}
+
+function copyCSSFiles(cb) {
+  src("css/**/*.css").pipe(dest("dist/css"));
+
+  cb();
+}
+
+function copyHTMLFiles(cb) {
+  src("index.dist.html").pipe(rename("index.html")).pipe(dest("dist"));
+
+  cb();
+}
+
+exports.default = parallel(
+  jsBundle,
+  copyVendorFiles,
+  copyCSSFiles,
+  copyHTMLFiles,
+);
